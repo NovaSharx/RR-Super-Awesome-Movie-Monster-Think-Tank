cfda82f639d6f91d17c6ac5554cedcdfc3de96ea
Davin <davin-dj@hotmail.com>
2022-11-25 18:19:53 -0500

    Initializing Project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import AboutUs from './components/AboutUs'
 import Packages from './components/Packages'
 
